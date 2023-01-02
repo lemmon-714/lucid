ed3a0e9f4f42d07688ba8f2ccd1d77058f9294db
Chris M. Hiatt <chris.hiatt@protonmail.com>
2023-01-02 16:57:09 +0100

    add mapRecords

--- src/plutus/types/mapRecord.ts
+++ src/plutus/types/mapRecord.ts
@@ -1,26 +1,34 @@
 import { assert } from "https://deno.land/std@0.167.0/testing/asserts.ts";
-import { Generators, genName, genNonNegative } from "../../mod.ts";
+import { fromHex, Generators, genNonNegative, toHex } from "../../mod.ts";
+import { PByteString } from "./bytestring.ts";
 import { PConstanted, PData, PLifted, PType, RecordOf } from "./type.ts";
 
-export class PRecord<PFields extends PData>
-  implements PType<Array<PConstanted<PFields>>, RecordOf<PLifted<PFields>>> {
+export class PMapRecord<PFields extends PData>
+  implements
+    PType<Map<string, PConstanted<PFields>>, RecordOf<PLifted<PFields>>> {
   constructor(
     public pfields: RecordOf<PFields>,
-  ) {}
+  ) {
+    for (const key in pfields) {
+      try {
+        toHex(fromHex(key));
+      } catch (e) {
+        throw new Error(`PMapRecord: key is not a valid hex string: ${key}`);
+      }
+    }
+  }
 
   public plift = (
-    l: Array<PConstanted<PFields>>,
+    m: Map<string, PConstanted<PFields>>,
   ): RecordOf<PLifted<PFields>> => {
     assert(
-      l instanceof Array,
-      `Record.plift: expected List: ${l}`,
+      m instanceof Map,
+      `MapRecord.plift: expected Map: ${m}`,
     );
     const r: Record<string, PLifted<PFields>> = {};
 
-    const pfields = Object.entries(this.pfields);
-    l.forEach((value, i) => {
-      const key = pfields[i][0];
-      const pvalue = pfields[i][1];
+    m.forEach((value, key) => {
+      const pvalue = this.pfields[key];
       r[key] = pvalue.plift(value);
     });
     return r;
@@ -28,38 +36,39 @@ export class PRecord<PFields extends PData>
 
   public pconstant = (
     data: RecordOf<PLifted<PFields>>,
-  ): Array<PConstanted<PFields>> => {
-    assert(data instanceof Object, `PRecord.pconstant: expected Object`);
+  ): Map<string, PConstanted<PFields>> => {
+    assert(data instanceof Object, `PMapRecord.pconstant: expected Object`);
 
     const pfieldsNames = Object.getOwnPropertyNames(this.pfields);
     const dataFieldsNames = Object.getOwnPropertyNames(data);
     assert(
       pfieldsNames.toString() === dataFieldsNames.toString(),
-      "PRecord.pconstant: fields mismatch",
+      "PMapRecord.pconstant: fields mismatch",
     );
 
-    const l = new Array<PConstanted<PFields>>();
+    const m = new Map<string, PConstanted<PFields>>();
     Object.entries(data).forEach(([key, value]) => {
       const pfield = this.pfields[key];
       assert(pfield, `field not found: ${key}`);
-      l.push(pfield.pconstant(value) as PConstanted<PFields>);
+      m.set(key, pfield.pconstant(value) as PConstanted<PFields>);
     });
-    return l;
+    return m;
   };
 
   static genPType(
     gen: Generators,
     maxDepth: number,
     maxLength: number,
-  ): PRecord<PData> {
+  ): PMapRecord<PData> {
     const pfields: RecordOf<PData> = {};
     const maxi = genNonNegative(maxLength);
+    const pbytes = new PByteString();
     for (let i = 0; i < maxi; i++) {
-      const key = genName();
+      const key = pbytes.genData();
       const pvalue = gen.generate(maxDepth, maxLength);
       pfields[key] = pvalue;
     }
-    return new PRecord(pfields);
+    return new PMapRecord(pfields);
   }
 
   public genData = (): RecordOf<PLifted<PFields>> => {
